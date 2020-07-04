cfcaf8943540e7d86b6b596902e2d7d882812d5d
BJ Lilley <bethjeanlilley@gmail.com>
2020-07-04 04:57:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,14 +17,12 @@ class Triangle
     elsif (@side_1 + @side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
       raise TriangleError
     else 
-      if 
-      (@side_1 == @side_2) && (@side_2 == @side_3)
-      :equilateral 
-    elsif (@side_3 != @side_2) || (@side_3 != @side_1 ) || (@side_1 != @side_2)
-      :scalene
-     
-    elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_3 == @side_1)
-      :isosceles
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
     
     end 
   end 
